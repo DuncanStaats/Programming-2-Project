476ac59f88323b250209197bcf8bd608fcfc071f
staatsd <staatsd@wlhs.wlwv.k12.or.us>
2025-04-29 09:53:33 -0700

    KennyYipFirstStep

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import pygame 
+from sys import exit
 
-def main():
-    pass
+GAME_WIDTH = 650  #Change Later
+GAME_HIEGHT = 650
 
-if __name__ == '__main__':
-    main()
+pygame.init()
+window = pygame.display.set_mode((GAME_WIDTH, GAME_HIEGHT))
+pygame.display.set_caption("Duncan's Catching Fruit - Pygame")
+clock = pygame.time.Clock()
+
+player = pygame.Rect(150,150,50,50)
+
+def draw():
+    window.fill("blue") #can use rgb colors tuple (())
+    pygame.draw.rect(window, (2, 239, 238), player)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    draw()
+    pygame.display.update()
+    clock.tick(60)
