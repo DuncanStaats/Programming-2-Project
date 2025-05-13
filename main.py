15d7e3f1a25ec7f0d52910ae97c9d1a74f210759
staatsd <staatsd@wlhs.wlwv.k12.or.us>
2025-05-13 09:55:39 -0700

    Next step

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 GAME_WIDTH = 512  #Change Later, to file size if you want the full background
 GAME_HEIGHT = 512
+TILE_SIZE = 32
 
 PLAYERX = GAME_WIDTH / 2
 PLAYERY = GAME_HEIGHT / 2
@@ -13,9 +14,10 @@ PLAYER_JUMP_WIDTH = 52
 PLAYER_JUMP_HEIGHT = 60
 PLAYER_DISTANCE = 5
 
-GRAVITY = 1
-PLAYER_VELOCITY_Y = -10
-FLOOR_Y = GAME_HEIGHT * 3/4
+GRAVITY = 0.5
+FRICTION = 0.4
+PLAYER_VELOCITY_X = 5
+PLAYER_VELOCITY_Y = -11
 
 def load_image(image_name, scale=None):
     image = pygame.image.load(os.path.join("image", image_name))
@@ -27,6 +29,7 @@ player_image_right = load_image("Sprite_image_test.png", (PLAYER_WIDTH, PLAYER_H
 player_image_left = load_image("Sprite_image_test1.png", (PLAYER_WIDTH, PLAYER_HEIGHT))
 player_image_jump_right = load_image("Sprite_image_test2.png", (PLAYER_JUMP_WIDTH, PLAYER_JUMP_HEIGHT))
 player_image_jump_left = load_image("Sprite_image_test3.png", (PLAYER_JUMP_WIDTH, PLAYER_JUMP_HEIGHT))
+floor_tile_image = load_image("Lava Tile.png", (TILE_SIZE, TILE_SIZE))
 
 
 pygame.init()
@@ -38,41 +41,99 @@ class Player(pygame.Rect):
     def __init__(self):
         pygame.Rect.__init__(self, PLAYERX, PLAYERY, PLAYER_WIDTH, PLAYER_HEIGHT)
         self.image = player_image_right
+        self.velocity_x = 0
         self.velocity_y = 0
         self.direction ="right"
         self.jumping = False
 
     def update_image(self):
         if self.jumping:
-            self.width = PLAYER_JUMP_WIDTH
-            self.height = PLAYER_HEIGHT
             if self.direction == "right":
                 self.image = player_image_jump_right
             elif self.direction == "left":
                 self.image = player_image_jump_left
         else:
-            self.width = PLAYER_WIDTH
-            self.height = PLAYER_HEIGHT
             if self.direction == "right":
                 self.image = player_image_right
             elif self.direction == "left":
                 self.image = player_image_left
 
-player = Player()
+class Tile(pygame.Rect):
+    def __init__(self, x, y, image):
+        pygame.Rect.__init__(self, x, y, TILE_SIZE, TILE_SIZE)
+        self.image = image
+
+def create_map():
+    for i in range(4):
+        tile = Tile(player.x + i*TILE_SIZE, player.y + TILE_SIZE*2, floor_tile_image)
+        tiles.append(tile)
+
+    for i in range(16):
+        tile = Tile(i*TILE_SIZE, player.y + TILE_SIZE*5, floor_tile_image)
+        tiles.append(tile)
+
+    for i in range(3):
+        tile = Tile(TILE_SIZE*3, (i+10)*TILE_SIZE, floor_tile_image)
+        tiles.append(tile)
+
+def check_tile_collision():
+    for tile in tiles:
+        if player.colliderect(tile):
+            return tile
+    return None
+    
+def check_tile_collisionx():
+    tile = check_tile_collision()
+    if tile is not None:
+        if player.velocity_x < 0:
+            player.x = tile.x + tile.width
+        elif player.velocity_x > 0:
+            player.x = tile.x - player.width
+        player.velocity_x = 0
+
+def check_tile_collisiony():
+    tile = check_tile_collision()
+    if tile is not None:
+        if player.velocity_y < 0:
+            player.y = tile.y + tile.height
+        elif player.velocity_y > 0:
+            player.y = tile.y - player.height
+            player.jumping = False
+        player.velocity_y = 0
 
 def move():
+    if player.direction == "left" and player.velocity_x < 0:
+        player.velocity_x += FRICTION
+    elif player.direction == "right" and player.velocity_x > 0:
+        player.velocity_x -= FRICTION
+    else:
+        player.velocity_x = 0
+
+    player.x += player.velocity_x
+    if player.x < 0:
+        player.x = 0
+    elif player.x + player.width > GAME_WIDTH:
+        player.x = GAME_WIDTH - player.width
+
+    check_tile_collisionx()
+
     player.velocity_y += GRAVITY
     player.y += player.velocity_y
-
-    if player.y + player.height > FLOOR_Y:
-        player.y = FLOOR_Y - player.height
-        player.jumping = False
+    check_tile_collisiony()
 
 def draw():
     window.fill("blue") #can use rgb colors tuple (())
     window.blit(background_image, (0,0)) #Order matters, image after #change position by changing numbers'
+
+    for tile in tiles:
+        window.blit(tile.image, tile)
+
     player.update_image()
     window.blit(player.image, player)
+
+player = Player()
+tiles = []
+create_map()
 
 while True:
     for event in pygame.event.get():
@@ -85,10 +146,10 @@ while True:
         player.velocity_y = PLAYER_VELOCITY_Y
         player.jumping = True
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        player.x = max(player.x - PLAYER_DISTANCE, 0)
+        player.velocity_x = -PLAYER_VELOCITY_X
         player.direction = "left"
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        player.x = min(player.x + PLAYER_DISTANCE, GAME_WIDTH - player.width)
+        player.velocity_x = PLAYER_VELOCITY_X
         player.direction = "right"
 
     move()
